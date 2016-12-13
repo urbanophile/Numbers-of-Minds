e7752448fb985a001678ab3f33720300d2fc8dc5
jackwm <jack@contraflo.ws>
2016-12-13 15:47:23 +1100

    The old files

---
+++ old/assets/js/vars.js
@@ -0,0 +1,8 @@
+/*
+	Minimaxing by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+var someVariable="SomeValue";
+document.getElementById("demo").innerHTML = someVariable;
